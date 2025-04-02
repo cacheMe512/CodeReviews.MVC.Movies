f8b7bc4015e5c0d3f39279697735feacaac15a34
Kornel Filipiak <kornelf048@gmail.com>
2025-04-02 08:09:54 -0400

    initial commit

---
+++ MVC.Movies.cacheMe512/MVC.Movies.cacheMe512/Data/MoviesContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC.Movies.cacheMe512.Data;
+
+public class MoviesContext : DbContext
+{
+    public MoviesContext(DbContextOptions<MoviesContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<MVC.Movies.cacheMe512.Models.Movie> Movie { get; set; } = default!;
+}
---
+++ MVC.Movies.cacheMe512/MVC.Movies.cacheMe512/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MVC.Movies.cacheMe512.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
